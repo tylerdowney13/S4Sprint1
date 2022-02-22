5256285abb3e89a21f73056c636b66eec66343e0
tylerdowney13 <tylerdowney83@gmail.com>
2022-02-22 14:50:36 -0330

    Second commit (added tournament class and testing)

--- src/main/java/com/example/Sprint1/Sprint1Server/Sprint1ServerApplication.java
+++ src/main/java/com/example/Sprint1/Sprint1Server/Sprint1ServerApplication.java
@@ -1,14 +1,81 @@
 package com.example.Sprint1.Sprint1Server;
 
 
+import com.example.Sprint1.Sprint1Server.dao.MemberAccessService;
+import com.example.Sprint1.Sprint1Server.dao.TournamentAccessService;
+import com.example.Sprint1.Sprint1Server.model.Member;
+import com.example.Sprint1.Sprint1Server.model.Tournament;
+import com.example.Sprint1.Sprint1Server.service.TournamentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 @SpringBootApplication
 public class Sprint1ServerApplication {
 
 	public static void main(String[] args) {
+
+		// Create members for database
+		Member m1 = new Member("Tyler Downey", "25 Baker Street", "tyler.downey@gmail.com",
+				"709-690-1337", "2022-02-21", 5, "family",
+				new ArrayList<String>(Arrays.asList("Tyler Downey", "Scott Downey")), new ArrayList<String>(Arrays.asList("PGA Tour 2022")),
+				new ArrayList<String>(Arrays.asList("PGA Tour 2021")), new ArrayList<String>(Arrays.asList("PGA Tour 2023"))
+		);
+
+		Member m2 = new Member("Scott Downey", "25 Baker Street", "scott.downey@gmail.com",
+				"709-690-4574", "2022-02-23", 5, "family",
+				new ArrayList<String>(Arrays.asList("Tyler Downey", "Scott Downey")), new ArrayList<String>(Arrays.asList("PGA Tour 2022")),
+				new ArrayList<String>(Arrays.asList("PGA Tour 2021")), new ArrayList<String>(Arrays.asList("PGA Tour 2023"))
+		);
+
+		Member m3 = new Member("Dryden Bussey", "3 Westbrook Landing", "dryden.bussey@gmail.com",
+				"709-690-1234", "2022-02-19", 12, "trial",
+				new ArrayList<String>(Arrays.asList()), new ArrayList<String>(Arrays.asList("PGA Tour 2022")),
+				new ArrayList<String>(Arrays.asList("PGA Tour 2021")), new ArrayList<String>(Arrays.asList("PGA Tour 2023"))
+		);
+
+		Member m4 = new Member("Leah Gregory", "10 Canada Drive", "leah.gregory@gmail.com",
+				"709-691-4567", "2022-02-01", 12, "special",
+				new ArrayList<String>(Arrays.asList()), new ArrayList<String>(Arrays.asList("PGA Tour 2022")),
+				new ArrayList<String>(Arrays.asList("PGA Tour 2021")), new ArrayList<String>(Arrays.asList("PGA Tour 2023"))
+		);
+
+
+		// Create member service
+		MemberAccessService memberAccessService = new MemberAccessService();
+
+		// Add members
+		memberAccessService.insertMember(m1);
+		memberAccessService.insertMember(m2);
+		memberAccessService.insertMember(m3);
+		memberAccessService.insertMember(m4);
+
+
+		// Create tournaments for database
+		Tournament t1 = new Tournament("2021-03-30", "2021-03-31", "St. John's", 50.00, 1000.00,
+				new ArrayList<String>(Arrays.asList("Tyler Downey", "Scott Downey", "Dryden Bussey", "Leah Gregory")),
+				new ArrayList<String>(Arrays.asList("Dryden Bussey", "Scott Downey", "Leah Gregory", "Tyler Downey")));
+
+		Tournament t2 = new Tournament("2022-03-30", "2022-03-31", "St. John's", 100.00, 2000.00,
+				new ArrayList<String>(Arrays.asList("Tyler Downey", "Scott Downey", "Dryden Bussey", "Leah Gregory")),
+				new ArrayList<String>(Arrays.asList("Dryden Bussey", "Scott Downey", "Leah Gregory", "Tyler Downey")));
+
+		Tournament t3 = new Tournament("2023-03-30", "2023-03-31", "St. John's", 200.00, 3000.00,
+				new ArrayList<String>(Arrays.asList("Tyler Downey", "Scott Downey", "Dryden Bussey", "Leah Gregory")),
+				new ArrayList<String>(Arrays.asList("Dryden Bussey", "Scott Downey", "Leah Gregory", "Tyler Downey")));
+
+		// Create tournament service
+		TournamentAccessService tournamentAccessService = new TournamentAccessService();
+
+		// Add tournaments
+		tournamentAccessService.insertTournament(t1);
+		tournamentAccessService.insertTournament(t2);
+		tournamentAccessService.insertTournament(t3);
+
+
 		SpringApplication.run(Sprint1ServerApplication.class, args);
 	}
 }
--- src/test/java/com/example/Sprint1/Sprint1Server/Sprint1ServerApplicationTests.java
+++ src/test/java/com/example/Sprint1/Sprint1Server/Sprint1ServerApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.Sprint1.Sprint1Server;
 
+import com.example.Sprint1.Sprint1Server.model.Member;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootTest
 class Sprint1ServerApplicationTests {
 
+	/// Member Tests ///
+	// Initial member
+	Member m1 = new Member("Tyler Downey", "25 Baker Street", "tyler.downey@gmail.com",
+			"709-690-1337", "2022-02-21", 3, "normal",
+			new ArrayList<String>(Arrays.asList("Tyler Downey", "Scott Downey")), new ArrayList<String>(Arrays.asList("PGA Tour 2022")),
+			new ArrayList<String>(Arrays.asList("PGA Tour 2021")), new ArrayList<String>(Arrays.asList("PGA Tour 2023"))
+	);
+
 	@Test
-	void contextLoads() {
+	public void testMemberGetMethods() {
+		Assertions.assertEquals("Tyler Downey", m1.getMemberName());
+		Assertions.assertEquals("25 Baker Street", m1.getMemberAddress());
+		Assertions.assertEquals("tyler.downey@gmail.com", m1.getMemberEmail());
+		Assertions.assertEquals("709-690-1337", m1.getMemberPhone());
+		Assertions.assertEquals(3, m1.getMembershipDuration());
+		Assertions.assertEquals("normal", m1.getMembershipType());
+		Assertions.assertEquals("Tyler Downey", m1.getMembershipFamilyMembers().get(0));
+		Assertions.assertEquals("PGA Tour 2022", m1.getCurrentTournaments().get(0));
+		Assertions.assertEquals("PGA Tour 2021", m1.getPastTournaments().get(0));
+		Assertions.assertEquals("PGA Tour 2023", m1.getUpcomingTournaments().get(0));
 	}
+
+	/// Tournament Tests ///
 
 }
